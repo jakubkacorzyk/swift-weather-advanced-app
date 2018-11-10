45f786ed1a08bea5273d646551b7f2eb0787886a
Jakub <Jakub@macbook-air-radosaw.home>
2018-11-10 10:43:13 +0100

    Added search controller.

--- weatherAdvancedKacorzyk/weatherAdvancedKacorzyk/DayData.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DayData.swift
-//  weatherAdvancedKacorzyk
-//
-//  Created by Jakub  on 28/10/2018.
-//  Copyright © 2018 Jakub . All rights reserved.
-//
-
-import Foundation
--- weatherAdvancedKacorzyk/weatherAdvancedKacorzyk/InsertController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  InsertController.swift
-//  weatherAdvancedKacorzyk
-//
-//  Created by Jakub  on 09/11/2018.
-//  Copyright © 2018 Jakub . All rights reserved.
-//
-
-import UIKit
